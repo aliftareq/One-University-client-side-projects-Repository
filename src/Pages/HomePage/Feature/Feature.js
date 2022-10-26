fc7d833ced8a00695ee4f74877947d2fcf691f9e
aliftareq <aliftareq@gmail.com>
2022-10-26 15:00:08 +0600

    home route modified

--- src/Pages/HomePage/Feature/Feature.js
+++ src/Pages/HomePage/Feature/Feature.js
@@ -43,7 +43,7 @@ const Feature = () => {
                 </p>
             </div>
             <div className="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
-                <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+                <div className="flex flex-col justify-between p-5 border rounded shadow-sm bg-white">
                     <div>
                         <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                             <svg
@@ -73,7 +73,7 @@ const Feature = () => {
                         Learn more
                     </Link>
                 </div>
-                <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+                <div className="flex flex-col justify-between p-5 border rounded shadow-sm bg-white">
                     <div>
                         <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                             <svg
@@ -103,7 +103,7 @@ const Feature = () => {
                         Learn more
                     </Link>
                 </div>
-                <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+                <div className="flex flex-col justify-between p-5 border rounded shadow-sm bg-white">
                     <div>
                         <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                             <svg
@@ -133,7 +133,7 @@ const Feature = () => {
                         Learn more
                     </Link>
                 </div>
-                <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+                <div className="flex flex-col justify-between p-5 border rounded shadow-sm bg-white">
                     <div>
                         <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                             <svg
